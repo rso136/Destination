1bd4d221a60444bf540e7821f2d94cb03b84848e
Richard Oh <rso136@gmail.com>
2017-01-10 17:44:35 -0500

    Added error handling

--- public/assets/js/mainController.js
+++ public/assets/js/mainController.js
@@ -4,16 +4,20 @@ app.controller('mainCtrl', function($scope, $http, $sce) {
     $scope.destination;
     $scope.introduction = true;
 
+    $('#noEventsBar').hide();
+
     $scope.search = function() {
+
+        $('#noEventsBar').hide();
 
         $scope.introduction = false;
 
         $('#eventBar').empty();
 
-        $scope.infoDisplay = true;
-        $scope.weatherInfo = true;
-        $scope.trafficInfo = true;
-        $scope.forecastDisplay = true;
+        //$scope.infoDisplay = true;
+        //$scope.weatherInfo = true;
+        //$scope.trafficInfo = true;
+        //$scope.forecastDisplay = true;
         $scope.eventDisplay = true;
         $scope.eventful = true;
         searchTermA = $scope.origin;
@@ -32,12 +36,25 @@ app.controller('mainCtrl', function($scope, $http, $sce) {
 
         $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + searchTermB + '&APPID=445ee2cab6664981c9d3ccdd572f246b').then(function(response) {
 
+            $scope.weatherErr = false;
+            $scope.infoDisplay = true;
+            $scope.weatherInfo = true;
+            $scope.trafficInfo = true;
+            $scope.forecastDisplay = true;
+
             var K = response.data.main.temp;
             var F = (K - 273.15) * 1.80 + 32;
-                
+            //console.log(response);
+
             $scope.temp = Math.round(F);
             $scope.forecast = response.data.weather[0].description;
 
+        }, function(response) {
+            $scope.weatherErr = true;
+            $scope.infoDisplay = false;
+            $scope.weatherInfo = false;
+            $scope.trafficInfo = false;
+            $scope.forecastDisplay = false;
         });
 
         $http.get('http://api.openweathermap.org/data/2.5/forecast?q=' + searchTermB + '&APPID=445ee2cab6664981c9d3ccdd572f246b').then(function(response) {
@@ -46,11 +63,11 @@ app.controller('mainCtrl', function($scope, $http, $sce) {
             $scope.fiveDates = [];
             $scope.fiveConditions = [];
 
-            console.log(response);
+            //console.log(response);
 
             var forecastLength = response.data.list.length;
 
-            console.log(forecastLength);
+           // console.log(forecastLength);
 
             for (var i = 0; i < forecastLength; i++) {
 
@@ -69,9 +86,11 @@ app.controller('mainCtrl', function($scope, $http, $sce) {
                 }
             }
 
-                console.log($scope.fiveForecast);
-                console.log($scope.fiveDates);
+                //console.log($scope.fiveForecast);
+                //console.log($scope.fiveDates);
 
+        }, function(response){
+            $scope.weatherErr = true;
         });
 
         $http.jsonp(url, {jsonpCallbackParam: 'callback'}).success(function(data){
@@ -85,69 +104,78 @@ app.controller('mainCtrl', function($scope, $http, $sce) {
 
         EVDB.API.call("/events/search", oArgs, function(oData) {
 
-            var arrayLength = oData.events.event.length;
-            var imgSrc;
-            var eventTitle;
+            //console.log(oData);
 
-            $('#eventBar').append("<h2 id='eventHeading'>Upcoming events in " + searchTermB + "</h2>");
-
-            if (oData.events.total_items == "1") {
-
-                var imgCheckAlt = oData.events.event.image;
-                var titleCheckAlt = oData.events.event.title;
-
-                if (imgCheckAlt == null) {
-                    imgSrc = "assets/images/no_image.png"
-                }
-                else {
-                    imgSrc = oData.events.event.image.medium.url;
-                }
-
-                if (titleCheckAlt == null) {
-                    eventTitle = 'Unnamed Event'; 
-                }
-                else {
-                    eventTitle = oData.events.event.title;  
-                }
-
-                var eventDateAlt = oData.events.event.start_time.substring(0, 10);
-                var eventVenueAlt = oData.events.event.venue_name;
-                var eventAddressAlt = oData.events.event.venue_address;
-                var eventUrlAlt = oData.events.event.url;
-                
-                $('#eventBar').append("<div class='eventBoxes col-md-4 col-xs-12'><p><b>" + eventTitle + "</b></p><a href=" + "'" + eventUrlAlt + "'" + "><img src=" + "'" + imgSrc + "'" + " height='150' width='150'></a><p>Date: " + eventDateAlt + "</p><p> " + eventVenueAlt +"</p><p>" + eventAddressAlt + "</p><a href=" + "'" + eventUrlAlt + "'" + " target='_blank'>More Info</a></div>");
+            if (oData.events == null) {
+                console.log('No events found');
+                $('#noEventsBar').show();
             }
+            
+            if (oData.events != null) {
+                
+                var arrayLength = oData.events.event.length;
+                var imgSrc;
+                var eventTitle;
 
-            if (oData.events.total_items != "1") {
+                $('#eventBar').append("<h2 id='eventHeading'>Upcoming events in " + searchTermB + "</h2>");
 
-                for (var i = 0; i < arrayLength; i++) {
+                if (oData.events.total_items == "1") {
 
-                    var imgCheck = oData.events.event[i].image;
-                    var titleCheck = oData.events.event[i].title;
+                    var imgCheckAlt = oData.events.event.image;
+                    var titleCheckAlt = oData.events.event.title;
 
-                    if (imgCheck == null) {
+                    if (imgCheckAlt == null) {
                         imgSrc = "assets/images/no_image.png"
                     }
                     else {
-                        imgSrc = oData.events.event[i].image.medium.url;
+                        imgSrc = oData.events.event.image.medium.url;
                     }
 
-                    if (titleCheck == null) {
-                        eventTitle = 'Unnamed Event';
+                    if (titleCheckAlt == null) {
+                        eventTitle = 'Unnamed Event'; 
                     }
                     else {
-                        eventTitle = oData.events.event[i].title;
+                        eventTitle = oData.events.event.title;  
                     }
 
-                    var eventDate = oData.events.event[i].start_time.substring(0, 10);
-                    var eventVenue = oData.events.event[i].venue_name;
-                    var eventAddress = oData.events.event[i].venue_address;
-                    var eventUrl = oData.events.event[i].url;
-
-                    $('#eventBar').append("<div class='eventBoxes col-md-4 col-xs-12'><p><b>" + eventTitle + "</b></p><a href=" + "'" + eventUrl + "'" + "><img src=" + "'" + imgSrc + "'" + " height='150' width='150'></a><p>Date: " + eventDate + "</p><p> " + eventVenue +"</p><p>" + eventAddress + "</p><a href=" + "'" + eventUrl + "'" + " target='_blank'>More Info</a></div>");
+                    var eventDateAlt = oData.events.event.start_time.substring(0, 10);
+                    var eventVenueAlt = oData.events.event.venue_name;
+                    var eventAddressAlt = oData.events.event.venue_address;
+                    var eventUrlAlt = oData.events.event.url;
+                    
+                    $('#eventBar').append("<div class='eventBoxes col-md-4 col-xs-12'><p><b>" + eventTitle + "</b></p><a href=" + "'" + eventUrlAlt + "'" + "><img src=" + "'" + imgSrc + "'" + " height='150' width='150'></a><p>Date: " + eventDateAlt + "</p><p> " + eventVenueAlt +"</p><p>" + eventAddressAlt + "</p><a href=" + "'" + eventUrlAlt + "'" + " target='_blank'>More Info</a></div>");
                 }
-            }
 
+                if (oData.events.total_items != "1") {
+
+                    for (var i = 0; i < arrayLength; i++) {
+
+                        var imgCheck = oData.events.event[i].image;
+                        var titleCheck = oData.events.event[i].title;
+
+                        if (imgCheck == null) {
+                            imgSrc = "assets/images/no_image.png"
+                        }
+                        else {
+                            imgSrc = oData.events.event[i].image.medium.url;
+                        }
+
+                        if (titleCheck == null) {
+                            eventTitle = 'Unnamed Event';
+                        }
+                        else {
+                            eventTitle = oData.events.event[i].title;
+                        }
+
+                        var eventDate = oData.events.event[i].start_time.substring(0, 10);
+                        var eventVenue = oData.events.event[i].venue_name;
+                        var eventAddress = oData.events.event[i].venue_address;
+                        var eventUrl = oData.events.event[i].url;
+
+                        $('#eventBar').append("<div class='eventBoxes col-md-4 col-xs-12'><p><b>" + eventTitle + "</b></p><a href=" + "'" + eventUrl + "'" + "><img src=" + "'" + imgSrc + "'" + " height='150' width='150'></a><p>Date: " + eventDate + "</p><p> " + eventVenue +"</p><p>" + eventAddress + "</p><a href=" + "'" + eventUrl + "'" + " target='_blank'>More Info</a></div>");
+                    }
+                }                
+            }
         });
     }
 });
